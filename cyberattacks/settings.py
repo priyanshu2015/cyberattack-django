089ad911415aa429f262820e03acbf31c2fe6296
Priyanshu Gupta <priyanshu.gupta2008@gmail.com>
2020-10-10 17:50:17 +0530

    whitenoise

--- cyberattacks/settings.py
+++ cyberattacks/settings.py
@@ -53,6 +53,10 @@ MIDDLEWARE = [
     #'firstcyberattacksapp.multiPort.MultiPortMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
