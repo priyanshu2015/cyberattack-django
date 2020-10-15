1a4c1e45a2bdea3300638357bcff0b73a0effe08
Priyanshu Gupta <priyanshu.gupta2008@gmail.com>
2020-10-15 12:28:07 +0530

    adding login csrf

--- firstcyberattacksapp/views.py
+++ firstcyberattacksapp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+domain = "herokuapp.com"
 def index(request):
     # num_visits = request.session.get('num_visits', 0)
     # request.session['num_visits'] = num_visits + 1
     # context = {
     #     'num_visits': num_visits,
     # }
-    return render(request, 'firstcyberattacksapp/index.html')# , context=context)
+    response = render(request, 'firstcyberattacksapp/index.html')
+    response.set_cookie('city', 'haridwar', domain="."+domain)
+    # return render(request, 'firstcyberattacksapp/index.html')# , context=context)
+    return response
