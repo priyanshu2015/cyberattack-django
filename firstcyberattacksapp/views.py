3f28f3f3d9eb7f4e5c33740b00e41fbb25409f74
Priyanshu Gupta <priyanshu.gupta2008@gmail.com>
2020-10-15 12:41:19 +0530

    login csrf

--- firstcyberattacksapp/views.py
+++ firstcyberattacksapp/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-domain = "ecommerce-mac2.herokuapp.com"
+# domain = "herokuapp.com"
 def index(request):
     # num_visits = request.session.get('num_visits', 0)
     # request.session['num_visits'] = num_visits + 1
     # context = {
     #     'num_visits': num_visits,
     # }
+    arr = request.get_host().split(':')[0].split('.')
+    # if we are at subdomain page right now
+    # we should delete subdomain using:
+    # arr.pop(0)
+    domain = ".".join(arr)
     response = render(request, 'firstcyberattacksapp/index.html')
     response.set_cookie('city', 'haridwar', domain="."+domain)
     # return render(request, 'firstcyberattacksapp/index.html')# , context=context)
